460c26ebdd8e659de5022e6f923a8c0a5012ae38
Juliano Rodrigues <julianokennedy11@gmail.com>
2023-11-20 20:58:09 -0300

    Trocando de pagina quando aperta na div.  e melhorias design das divs com imagens'

---
+++ main.js
@@ -0,0 +1,23 @@
+var btAzul = document.getElementById('btAzul')
+
+btAzul.onclick = function myFunction() {
+    location.href="outrasPags/lixoAzul.html"
+  }
+
+var btVerm = document.getElementById('btVermelho')
+
+btVerm.onclick = function myFunction() {
+    alert('clicou verm');
+  }
+
+var btAmar = document.getElementById('btAmarelo')
+
+btAmar.onclick = function myFunction() {
+    alert('clicou amarelo');
+  }
+
+var btVerde = document.getElementById('btVerde')
+
+btVerde.onclick = function myFunction() {
+    alert('clicou verde');
+  }
